607bb0e061efc90c794922ffcf9915ebf6cbe07b
bosseschum <accounts@bschum.de>
2026-02-18 13:51:12 +0100

    Board functionality implemented

--- lib/board.rb
+++ lib/board.rb
@@ -10,4 +10,57 @@ class Board
     row = (rows - 1).downto(0).find { |row| @board[row][col - 1] == '' }
     @board[row][col - 1] = marker
   end
+
+  def valid_move?(col)
+    board[0][col - 1] == ''
+  end
+
+  def winner?
+    check_horizontal || check_vertical || check_diagonal_ascending || check_diagonal_descending
+  end
+
+  def board_full?
+    @board.none? { |slot| slot == '' }
+  end
+
+  private
+
+  def check_horizontal
+    @board.each do |row|
+      row.each_cons(4) do |window|
+        return true if window.all? { |cell| cell == window.first } && !window.first.empty?
+      end
+    end
+    false
+  end
+
+  def check_vertical
+    (0..6).each do |col|
+      column = @board.map { |row| row[col] }
+      column.each_cons(4) do |window|
+        return true if window.all? { |cell| cell == window.first } && !window.first.empty?
+      end
+    end
+    false
+  end
+
+  def check_diagonal_ascending
+    (3..5).each do |row|
+      (0..3).each do |col|
+        window = 4.times.collect { |i| @board[row - i][col + i] }
+        return true if window.all? { |cell| cell == window.first } && !window.first.empty?
+      end
+    end
+    false
+  end
+
+  def check_diagonal_descending
+    (0..2).each do |row|
+      (0..3).each do |col|
+        window = 4.times.collect { |i| @board[row + i][col + i] }
+        return true if window.all? { |cell| cell == window.first } && !window.first.empty?
+      end
+    end
+    false
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -21,14 +21,98 @@ describe Board do
   end
 
   describe '#valid_move?' do
-    pending 'Not implemented'
+    context 'when the chosen column has space' do
+      it 'returns true' do
+        expect(board.valid_move?(1)).to eq(true)
+      end
+
+      it 'returns true' do
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        expect(board.valid_move?(1)).to eq(true)
+      end
+    end
+
+    context 'when the chosen column is full' do
+      it 'returns false' do
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        expect(board.valid_move?(1)).to eq(false)
+      end
+    end
   end
 
   describe '#winner?' do
-    pending 'Not implemented'
+    context 'when four of the same chips are in a row' do
+      it 'returns true horizontally' do
+        board.drop_chip(1, 'X')
+        board.drop_chip(2, 'X')
+        board.drop_chip(3, 'X')
+        board.drop_chip(4, 'X')
+        expect(board.winner?).to eq(true)
+      end
+
+      it 'returns true vertically' do
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'X')
+        expect(board.winner?).to eq(true)
+      end
+
+      it 'returns true ascending diagonally' do
+        board.drop_chip(1, 'X')
+        board.drop_chip(2, 'O')
+        board.drop_chip(2, 'X')
+        board.drop_chip(3, 'O')
+        board.drop_chip(3, 'O')
+        board.drop_chip(3, 'X')
+        board.drop_chip(4, 'X')
+        board.drop_chip(4, 'O')
+        board.drop_chip(4, 'O')
+        board.drop_chip(4, 'X')
+        expect(board.winner?).to eq(true)
+      end
+
+      it 'returns true descendind diagonally' do
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'O')
+        board.drop_chip(1, 'X')
+        board.drop_chip(1, 'O')
+        board.drop_chip(2, 'O')
+        board.drop_chip(2, 'X')
+        board.drop_chip(2, 'O')
+        board.drop_chip(3, 'O')
+        board.drop_chip(3, 'O')
+        board.drop_chip(4, 'O')
+        expect(board.winner?).to eq(true)
+      end
+    end
+
+    context 'when no four of the same chip are in a row' do
+      it 'returns false' do
+        expect(board.winner?).to eq(false)
+      end
+    end
   end
 
-  describe '#full?' do
-    pending 'Not implemented'
+  describe '#board_full?' do
+    context 'when all slots are taken' do
+      it 'returns true' do
+        board.board = [
+          %w[X X O O O X O], %w[O O X X X O X],
+          %w[X O X O X O X], %w[O O X X O O X],
+          %w[X X O O X O X], %w[X O X O O X O]
+        ]
+        expect(board.board.none? { |slot| slot == '' }).to eq(true)
+      end
+    end
   end
 end
